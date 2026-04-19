ee39cf686c3b6d222fbacee207c0c42c098cbcc2
namni <nam.micro@outlook.com>
2026-04-19 21:23:53 +0800

    Imported libraries, added inputs and defined functions

--- intro.py
+++ intro.py
@@ -1,2 +1,16 @@
-Hello World 
-feature
+from math import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Asks for user inputs
+func = input("Enter a function in terms of x (e.g. x2, x^3 + 2, sin(x)): ")
+func = func.replace("^","**")
+lowerB = float(input("Lower bound a = "))
+upperB = float(input("Upper bound b = "))
+numRect = int(input("How many rectangles? n = "))
+method = input("Choose method (L=left, R=right, M=midpoint): ").upper()
+
+# Defined function
+def f(x):
+    return eval(func, {"x": x, **globals()})
+
